5a8f9c2d24ad77ef18abf70975247bd3d7f3d48e
Joaquin Arlettaz <jjoaquinarlettaz@gmail.com>
2022-02-16 12:51:16 -0300

    Starting with the project

---
+++ js/index.js
@@ -0,0 +1,34 @@
+const container = document.querySelector('.container');
+const resetButton = document.getElementById('resetButton');
+const cells = container.querySelectorAll('.cell');
+const cell = container.querySelector('.cell');
+
+let statusGame = false;
+let playerOne, playerTwo;
+
+const newGame = () => {
+    cells.forEach(element => element.textContent = '');
+    statusGame = false;
+}
+
+resetButton.addEventListener("click",newGame);
+
+const playing = () => {
+    statusGame = true;
+}
+
+playing();
+
+const printCell = (e) => {
+    console.log(e.target);
+    e.target.textContent ='0';
+}
+
+for (let cell in cells)  {
+    cells[cell].addEventListener("click",(e) => printCell(e));
+}
+
+
+
+
+
